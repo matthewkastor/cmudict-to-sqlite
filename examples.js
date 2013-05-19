022e32a6de3bc21b3bcc76df7ea490bdd8754c32
Matthew Kastor <matthewkastor@gmail.com>
2013-05-19 06:34:12 -0400

    add examples for new methods of CmudictDb

--- examples.js
+++ examples.js
@@ -1,20 +1,111 @@
+/*jslint
+    white: true,
+    node: true,
+    stupid: true,
+    nomen: true,
+    vars: true,
+    regexp: true
+*/
+
 var cmu = require('cmudict-to-sqlite');
 cmu = new cmu.CmudictDb();
+
 cmu.lookupWord('zebra', function (err, rows) {
-    console.log('lookupWord Results');
-    console.log(rows);
+    'use strict';
+    console.log('lookupWord Results for zebra');
+    if (err) { console.log(err); }
+    if (rows) { console.log(rows); }
 });
 cmu.lookupCode('ah1 p s ay1 z', function (err, rows) {
-    console.log('lookupCode Results');
-    console.log(rows);
+    'use strict';
+    console.log('lookupCode Results for ah1 p s ay1 z');
+    if (err) { console.log(err); }
+    if (rows) { console.log(rows); }
 });
-cmu.fuzzyLookupWord('zeb%', function (err, rows) {
-    console.log('fuzzyLookupWord Results');
-    console.log(rows);
+cmu.findPhoneme('%r ah1 p t%', function (err, rows) {
+    'use strict';
+    // finds words containing the sound "rupt" i.e. erupt and corrupt
+    console.log('findPhoneme Results for %r ah1 p t%');
+    if (err) { console.log(err); }
+    if (rows) { console.log(rows); }
 });
+
+// wrapping fuzzyLookupWord example for use below.
+function show (word) {
+    'use strict';
+    cmu.fuzzyLookupWord(word, function (err, rows) {
+        console.log('fuzzyLookupWord Results for ' + word);
+        if (err) { console.log(err); }
+        if (rows) { console.log(rows); }
+    });
+}
+show('zeb%');
+
 cmu.fuzzyLookupCode('%s ay1 z%', function (err, rows) {
-    console.log('fuzzyLookupCode Results');
-    console.log(rows);
+    'use strict';
+    console.log('fuzzyLookupCode Results for %s ay1 z%');
+    if (err) { console.log(err); }
+    if (rows) { console.log(rows); }
 });
-// do other stuff with the database . . .
-cmu.unload();
+ 
+cmu.addEntry('superfakeword', 'xo xo xo1', function (err, rows) {
+    'use strict';
+    console.log('addEntry Results for superfakeword, xo xo xo1');
+    if (err) { console.log(err); }
+    if (rows) {
+        console.log(rows);
+        show('superfake%');
+        /*jslint undef:true */
+        upword();
+        /*jslint undef:false */
+    }
+});
+
+// wrapping the updateWord example instead of embedding it in the addEntry
+// example. The sqlite3 module operates asynchronously.
+function upword () {
+    'use strict';
+    cmu.updateWord('superfakewordzzz', 'superfakeword', function (err, rows) {
+        console.log('updateWord Results for superfakewordzzz, superfakeword');
+        if (err) { console.log(err); }
+        if (rows) {
+            console.log(rows);
+            show('superfake%');
+            /*jslint undef:true */
+            upcode();
+            /*jslint undef:false */
+        }
+    });
+}
+
+// wrapping the updateCode example instead of embedding it.
+function upcode () {
+    'use strict';
+    cmu.updateCode('xo1 xo xo', 'xo xo xo1', function (err, rows) {
+        console.log('updateCode Results for xo1 xo xo, xo xo xo1');
+        if (err) { console.log(err); }
+        if (rows) {
+            console.log(rows);
+            show('superfake%');
+            /*jslint undef:true */
+            del();
+            /*jslint undef:false */
+        }
+    });
+}
+
+// wrapping the deleteEntry example instead of embedding it.
+function del () {
+    'use strict';
+    cmu.deleteEntry('superfakewordzzz', function (err, rows) {
+        console.log('deleteEntry Results for superfakewordzzz');
+        if (err) { console.log(err); }
+        if (rows) {
+            console.log(rows);
+            show('superfake%');
+            // do other stuff with the database, you'll have to move the unload
+            // call to wherever you're finished with the database.
+            cmu.unload();
+        }
+    });
+}
